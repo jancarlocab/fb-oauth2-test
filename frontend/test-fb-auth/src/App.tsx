f926a14a2861808957bbfeaea2147986bfadd423
Jan Carlo Cabrera <jlcabrera1@up.edu.ph>
2025-03-06 16:00:10 +0800

    added test website with endpoint calls

---
+++ frontend/test-fb-auth/src/App.tsx
@@ -0,0 +1,12 @@
+import FacebookLogin from "./components/FacebookLogin";
+
+const App = () => {
+  return (
+    <div>
+      <h1>FastAPI + React Facebook OAuth2</h1>
+      <FacebookLogin />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/test-fb-auth/src/components/FacebookLogin.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+
+const FacebookLogin = () => {
+  const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+  const handleFacebookLogin = () => {
+    window.location.href = `${apiBaseUrl}/auth/login`;
+  };
+
+  useEffect(() => {
+    // CATCH CALLBACK
+
+    // GET THE 'code' in the query params of the URL
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    // Call /callback
+    // get user info in this route!
+    if (code) {
+      fetch(`${apiBaseUrl}/auth/callback?code=${code}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("User Info:", data);
+        })
+        .catch((err) => console.error("Error during Facebook OAuth:", err));
+    }
+  }, []);
+
+  //TODO: Store token to storage
+
+  return (
+    <div>
+      <h1>Sign in with FB Below!</h1>
+      <button onClick={handleFacebookLogin}>Login with Facebook</button>
+    </div>
+  );
+};
+
+export default FacebookLogin;
